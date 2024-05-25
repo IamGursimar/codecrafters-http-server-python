4253ec028fb661beb2cabf0e84efda396bf2e214
IamGursimar <gursimarsingh195@gmail.com>
2024-05-26 07:51:23 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,15 @@ run ./your_server.sh in one terminal session, and nc -vz 127.0.0.1 4221 in anoth
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    conn, addr = server_socket.accept()  # wait for client
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            request_data = data.decode().split("\r\n")
+            #  ['GET / HTTP/1.1', 'Host: localhost:4221', '', '']
+            if not data:
+                break
+            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
